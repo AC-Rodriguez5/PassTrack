bd3dc70b941765fe805009c759a7b92a1f829f77
Rodriguez747 <ahronchristian.rodriguez@cvsu.edu.ph>
2026-08-04 15:37:32 +0800

    'try1'

--- backEnd/server.js
+++ backEnd/server.js
@@ -4,17 +4,7 @@ const User = require ('../middleware/user.middleware.js');
 const PORT = procss.env.PORT;
 app.use(express.json());
 
-const mongoose = require('mongoose');
-
-const MONGO_URI = process.env.MONGO_URI;
-
-//<-----connection to server and database 
-app.get('/', (req,res)=> {
-    res.send(`Hello ${PORT}`)
-})
-
-mongoose.connect(MONGO_URI).then(()=>{
-    console.log (`connected to database!`);
+con(`connected to database!`);
 }).catch(()=>{
     console.log(`failed to connect to database`);
 });
